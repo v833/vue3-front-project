f607884ec28979e0bb7534942fd0635902448a00
v833 <2507301541@qq.com>
2022-11-12 11:08:31 +0800

    feat: header-search

---
+++ src/components/header/header-search/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full">
+    <m-search v-model="searchValue"></m-search>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchValue = ref('')
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>header</div>
+  <div class="w-full bg-white border-b border-b-zinc-200 px-2 py-1">
+    <div class="flex items-center">
+      <img
+        class="h-4 cursor-pointer mr-2"
+        src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+        @click="$router.push('/')"
+        alt=""
+      />
+      <header-search />
+      <header-theme />
+      <header-my />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeaderSearch from './header-search/index.vue'
+import HeaderTheme from './header-theme.vue'
+import HeaderMy from './header-my.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+// const onLogoClick = () => {
+//   router.push('/')
+// }
+</script>
 
 <style lang="scss" scoped></style>
